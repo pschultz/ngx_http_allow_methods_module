8ba1ee7bc076ef2b7c870ca1b0b69b194661870d
Peter Schultz <schultz.peter@hotmail.com>
2013-02-20 13:40:59 +0100

    allow directing in if in location

--- ngx_http_allow_method_module.c
+++ ngx_http_allow_method_module.c
@@ -29,7 +29,7 @@ static ngx_int_t ngx_http_allow_method_init(ngx_conf_t *cf);
 static ngx_command_t  ngx_http_allow_method_commands[] = {
 
     { ngx_string("allow_methods"),
-      NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_1MORE,
+      NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_HTTP_LIF_CONF|NGX_CONF_1MORE,
       ngx_http_allow_method_set_allow_methods,
       NGX_HTTP_LOC_CONF_OFFSET,
       0,
